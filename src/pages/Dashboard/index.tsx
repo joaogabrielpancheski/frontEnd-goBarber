470dc5270f8ea073d2d29042c542a9560a33ebaf
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2020-06-05 22:03:23 -0300

    Implements profile page structure

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import 'react-day-picker/lib/style.css';
 import { isToday, isAfter, format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
 
@@ -145,7 +146,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
